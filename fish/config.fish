66b3dd4dd11736dd7de35da9642a4a1f0915edb8
Benny <peptoni0345@gmail.com>
2023-09-29 16:15:05 +0200

    New fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,35 +1,31 @@
 # Starship theme
 starship init fish | source
 
+# Remove Welcome message
+set fish_greeting
+
+# Dart path
+set PATH $PATH /opt/flutter/bin/
+
+# GPG keys
+set -gx GPG_TTY (tty)
+
 # Alias
 
 # ls, tree and cat replacements for the exa, exa --tree and ccat modern versions with extra config.
-
-alias ls='exa -hg --grid --group-directories-first --time-style=long-iso --ignore-glob="System Volume Information|?RECYCLE.BIN" --icons --git'
-
-alias ll='exa -lhg --grid --group-directories-first --time-style=long-iso --ignore-glob="System Volume Information|?RECYCLE.BIN" --icons --git'
-
-alias hls='exa --all -hg --group-directories-first --grid --time-style=long-iso --ignore-glob="System Volume Information|?RECYCLE.BIN" --icons --git'
-
-alias hll='exa --all -lhg --grid --group-directories-first --time-style=long-iso --ignore-glob="System Volume Information|?RECYCLE.BIN" --icons --git'
-
-alias dls='ls | grep '''
-
+alias ls='exa -hg --grid --time-style=long-iso --ignore-glob="System Volume Information|?RECYCLE.BIN" --icons --git'
+alias ll='exa -lhg --grid --time-style=long-iso --ignore-glob="System Volume Information|?RECYCLE.BIN" --icons --git'
+alias hls='exa --all -hg --grid --time-style=long-iso --ignore-glob="System Volume Information|?RECYCLE.BIN" --icons --git'
+alias hll='exa --all -lhg --grid --time-style=long-iso --ignore-glob="System Volume Information|?RECYCLE.BIN" --icons --git'
 alias tree='exa --tree -hg --time-style=long-iso --ignore-glob="System Volume Information|?RECYCLE.BIN" --icons --git'
-alias cat='ccat -G Keyword=teal -G Literal=brown -G Decimal=brown -G Plaintext=drakblue'
-alias killmyself='shutdown now'
+alias cat='bat --style plain'
+alias bcat='bat'
 
 # Alacritty Terminal Alias
 alias term='alacritty & disown'
 alias rterm='alacritty & disown | exit'
 
-# Aseprite
-alias aseprite='/Personal/Programacion/Unity/Herramientas\ Para\ GameMaking/Aseprite/Aseprite.exe & disown'
-
-set DOTNET_CLI_TELEMETRY_OPTOUT 0
-
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+# Logout GNOME SESSION
+alias  logoff='gnome-session-quit --no-prompt --logout'
 
 pfetch
